2986731451dcf45ef747e6d5517c1f4756cf91d0
BYUSAA <byuussaa@gmail.com>
2024-08-08 18:11:35 +0100

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
-"""
-Script that fetches the top 10 hot posts for a given subreddit.
-"""
-
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
+
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts listed for a given subreddit.
-    If the subreddit is not valid, prints None.
-    
-    Args:
-        subreddit (str): The name of the subreddit to query.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code == 200:
-            posts = response.json().get("data", {}).get("children", [])
-            if not posts:
-                print(None)
-            else:
-                for post in posts[:10]:
-                    print(post.get("data", {}).get("title"))
-        else:
-            print(None)
-    except Exception:
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
+
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
+
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
+    else:
         print(None)
