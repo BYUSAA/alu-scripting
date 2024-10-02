0278adf3de6d617c8b2b2242f369b5b8287a9905
BYUSAA <byuussaa@gmail.com>
2024-10-02 15:04:55 +0100

    new add

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""Module that fetches the top ten hot posts from a given subreddit."""
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the top 10 hot posts for a given subreddit.
-    
-    If the subreddit is valid, prints the titles of the posts. 
-    If not a valid subreddit, prints None.
-    """
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
+
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = f"https://reddit.com/r/{subreddit}.json"
-    response = requests.get(subreddit_url, headers=headers, allow_redirects=False)
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
 
     if response.status_code == 200:
         json_data = response.json()
@@ -24,13 +22,3 @@ def top_ten(subreddit):
             )
     else:
         print(None)
-
-
-# This block is for testing the function
-if __name__ == "__main__":
-    import sys  # Import sys module for command-line argument handling
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
