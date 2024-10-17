667239333323a4c5f905469bc2cfe8372b942fb8
BYUSAA <byuussaa@gmail.com>
2024-10-17 10:58:00 +0100

    update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-"""Print the titles of the first 10 hot posts for a given subreddit"""
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """The top ten titles for a given subreddit"""
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            json_data = response.json().get('data', {})
-            children = json_data.get('children', [])
-            
-            if len(children) == 0:
-                print(None)
-            else:
-                for post in children:
-                    print(post.get('data', {}).get('title', None))
-        else:
-            print(None)
-    except Exception:
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
+
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
+    else:
         print(None)
