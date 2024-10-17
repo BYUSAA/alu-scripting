49e08ec9f03727e7965eebff1f9608499f912840
BYUSAA <byuussaa@gmail.com>
2024-10-17 10:54:52 +0100

    update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""Print the titles of the first 10Hot Posts"""
+"""Print the titles of the first 10 hot posts for a given subreddit"""
 import requests
 
 
 def top_ten(subreddit):
-    """The top ten titles"""
+    """The top ten titles for a given subreddit"""
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(url, headers=headers)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                    json_data.get('data')
-                    .get('children')[i]
-                    .get('data')
-                    .get('title')
-                )
-    else:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            json_data = response.json().get('data', {})
+            children = json_data.get('children', [])
+            
+            if len(children) == 0:
+                print(None)
+            else:
+                for post in children:
+                    print(post.get('data', {}).get('title', None))
+        else:
+            print(None)
+    except Exception:
         print(None)
